4946b771c07fd019f0206c4594c97f9a0db833aa
rhnkbr <raihanakbar@protonmail.com>
2020-03-19 14:57:34 +0700

    a

--- coronasux.cpp
+++ coronasux.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include<string>
 
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    cout << "Input: ";
+    string data;
+    cin >> data;
+    cout << "Output: " << data << "\n\n";
+    return 0;
+}
