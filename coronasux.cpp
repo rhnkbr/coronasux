0f4a67e3b2d6aa24c7ab1c0bbe1a708cb41dfa47
rhnkbr <raihanakbar@protonmail.com>
2020-03-19 15:14:00 +0700

    okemantab

--- coronasux.cpp
+++ coronasux.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-#include<string>
-
 using namespace std;
-
-int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    cin >> data;
-    cout << "Output: " << data << "\n\n";
-    return 0;
-}
+int  main() {
+      int x[10],mod=0;
+      int *ptr;
+          cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
+          cout << "\n\n";
+           ptr = x;
+           for (int y=0; y < 10; y+=1) {
+               cout << "MASUKKAN BILANGAN NO. " << y+1 << ": ";
+               cin >> ptr[y];
+           }
+           cout << "\n \t BILANGAN GANJIL";
+          cout << "\n\n";
+           for (int y=0; y < 10; y+=1) {
+               mod = ptr[y] % 2;
+             if (mod != 0) {
+                 cout << " " << ptr[y] << " ";
+             }
+           }
+           cout << "\n\n";
+           cout << "\n \t BILANGAN GENAP";
+           cout << "\n\n";
+          for (int y=0; y < 10; y+=1) {
+              mod = ptr[y] % 2;
+             if (mod == 0) {
+                 cout << " " << ptr[y] << " ";
+             }
+          }
+         cout << "\n\n";
+         return 0;
+       }
