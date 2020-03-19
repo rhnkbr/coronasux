3f4b3d736efa17790fd6b9b23a5679d693e00264
Fadhil Hakim <59780168+fadhilhakim35@users.noreply.github.com>
2020-03-19 16:19:34 +0700

    Update coronasux.cpp

--- coronasux.cpp
+++ coronasux.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 using namespace std;
+
+Ganjil (int *mod) (int *mypointer) {
+	for (int z=0; z<5; z+=1) {
+		*mod = mypointer[z]%2;
+		if (*mod != 0) {
+			cout << " " << mypointer[z] << " ";
+		}
+	}
+}
+
+Genap (int *mod) (int *mypointer) {
+	for (int z=0; z<5;z+=1) {
+		mod = mypointer[z]%2;
+		if(mod == 0){
+			cout << " " << mypointer[z] << " ";
+		}
+	}
+}
+
 int  main() {
       int x[10],mod=0;
       int *ptr;
-          cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
-          cout << "\n\n";
-           ptr = x;
-           for (int y=0; y < 10; y+=1) {
-               cout << "MASUKKAN BILANGAN NO. " << y+1 << ": ";
-               cin >> ptr[y];
-           }
-           cout << "\n \t BILANGAN GANJIL";
-          cout << "\n\n";
-           for (int y=0; y < 10; y+=1) {
-               mod = ptr[y] % 2;
-             if (mod != 0) {
-                 cout << " " << ptr[y] << " ";
-             }
-           }
-           cout << "\n\n";
-           cout << "\n \t BILANGAN GENAP";
-           cout << "\n\n";
-          for (int y=0; y < 10; y+=1) {
-              mod = ptr[y] % 2;
-             if (mod == 0) {
-                 cout << " " << ptr[y] << " ";
-             }
-          }
-         cout << "\n\n";
-         return 0;
-       }
+      cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
+      cout << "\n\n";
+      ptr = x;
+      for (int y=0; y < 10; y+=1) {
+            cout << "MASUKKAN BILANGAN NO. " << y+1 << ": ";
+            cin >> ptr[y];
+      }
+      
+      cout << "\n \t BILANGAN GANJIL";
+      cout << "\n\n";
+      
+      Ganjil ()(&mod)(&mypointer);
+      
+      cout << "\n\n";
+      cout << "\n \t BILANGAN GENAP";
+      cout << "\n\n";
+          
+      Genap ()(&mod)(&mypointer);
+          
+      cout << "\n\n";
+      return 0;
+}
