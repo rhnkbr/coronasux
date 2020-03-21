157872ecac7cad32e496a1fcd0f1ad10bd1911a2
Fadhil Hakim <59780168+fadhilhakim35@users.noreply.github.com>
2020-03-22 06:21:05 +0700

    Update coronasux.cpp

--- coronasux.cpp
+++ coronasux.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
 using namespace std;
 
-Ganjil (int *mod) (int *ptr) {
-	for (int y=0; y<10; y+=1) {
-		*mod = ptr[y]%2;
-		if (*mod != 0) {
-			cout << " " << ptr[y] << " ";
-		}
-	}
+void Ganjil (int &mod) { 
+	mod = mod%2;
 }
 
-Genap (int *mod) (int *ptr) {
-	for (int y=0; y<10; y+=1) {
-		mod = ptr[y]%2;
-		if(mod == 0){
-			cout << " " << ptr[y] << " ";
-		}
-	}
+void Genap (int &mod) {
+	mod = mod%2;
 }
 
 int  main() {
-      int x[10], mod=0;
-      int *ptr;
-      cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
-      cout << "\n\n";
-      ptr = x;
-      for (int y=0; y < 10; y+=1) {
-            cout << "MASUKKAN BILANGAN NO. " << y+1 << ": ";
-            cin >> ptr[y];
-      }
+	int x[10], mod=0;
+      	int *ptr;
+      	cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
+      	cout << "\n\n";
+      	ptr = x;
+      	for (int y=0; y < 10; y+=1) {
+        	cout << "MASUKKAN BILANGAN NO. " << y+1 << " : ";
+        	cin >> ptr[y];
+      	}
       
-      cout << "\n \t BILANGAN GANJIL";
-      cout << "\n\n";
-      
-      Ganjil ()(&mod)(&ptr);
-      
-      cout << "\n\n";
-      cout << "\n \t BILANGAN GENAP";
-      cout << "\n\n";
-          
-      Genap ()(&mod)(&ptr);
-          
-      cout << "\n\n";
-      return 0;
+     	cout << "\n \t BILANGAN GANJIL";
+      	cout << "\n\n";
+      	for (int y=0; y<10; y+=1) {
+      		Ganjil (ptr[y]);
+      		if (ptr[y] != 0) {
+      			cout << " " << ptr[y] << " ";
+		}
+	}
+	
+      	cout << "\n\n";
+      	cout << "\n \t BILANGAN GENAP";
+      	cout << "\n\n";
+        for (int y=0; y<10; y+=1) {
+		Genap (ptr[y]);
+          	if (ptr[y] == 0) {
+			cout << " " << ptr[y] << " ";
+		}
+	}
+				
+      	cout << "\n\n";
+      	return 0;
 }
