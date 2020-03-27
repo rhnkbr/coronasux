65af591fa75688e4ce12af8b735298836397d795
Fadhil Hakim <59780168+fadhilhakim35@users.noreply.github.com>
2020-03-27 19:38:39 +0700

    Update coronasux.cpp

--- coronasux.cpp
+++ coronasux.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void Ganjil (int &mod) { 
-	mod = mod%2;
-}
-
-void Genap (int &mod) {
-	mod = mod%2;
-}
-
 int  main() {
 	int x[10], mod=0;
       	int *ptr;
       	cout << "\n\t PROGRAM PEMISAH BILANGAN GANJIL DAN GENAP";
       	cout << "\n\n";
       	ptr = x;
-      	for (int y=0; y < 10; y+=1) {
+      	for (int y=0; y<10; y+=1) {
         	cout << "MASUKKAN BILANGAN NO. " << y+1 << " : ";
         	cin >> ptr[y];
       	}
@@ -23,8 +15,8 @@ int  main() {
      	cout << "\n \t BILANGAN GANJIL";
       	cout << "\n\n";
       	for (int y=0; y<10; y+=1) {
-      		Ganjil (ptr[y]);
-      		if (ptr[y] != 0) {
+      		mod = ptr[y]%2;
+      		if (mod != 0) {
       			cout << " " << ptr[y] << " ";
 		}
 	}
@@ -33,8 +25,8 @@ int  main() {
       	cout << "\n \t BILANGAN GENAP";
       	cout << "\n\n";
         for (int y=0; y<10; y+=1) {
-		Genap (ptr[y]);
-          	if (ptr[y] == 0) {
+		mod = ptr[y]%2;
+          	if (mod == 0) {
 			cout << " " << ptr[y] << " ";
 		}
 	}
